d353b3dc8b8344c344ddc7fec53611f66b968244
RC_Student_lab <RC_Student_lab@10.0.0.21>
2024-09-26 19:59:25 +0000

    POE java project

---
+++ src/main/java/com/mycompany/poe1/Poe1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Poe1 {
+
+    public static void main(String[] args) {
+        
+         Scanner  scanner = new Scanner(System.in);
+        //declarations
+        String username;
+        String password;
+        String firstname;
+        String lastname;
+        //prompt the user to enter their details
+         System.out.print("enter username");
+ username = scanner.next();
+ System.out.print("enter firstname");
+ firstname = scanner.next();
+  System.out.print("lastname");
+ lastname = scanner.next();
+  
+
+    }
+}
---
+++ src/main/java/com/mycompany/poe1/Poe1Class.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poe1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Poe1Class {
+      //declaration
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    
+//the getter and setter methods
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    //construct username and password
+    
+
+    
+}
